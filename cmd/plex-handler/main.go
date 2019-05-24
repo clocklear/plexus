90aede1b404f4361ba0f31b4069e24781ffcf9b4
Cory Locklear <corylocklear@gmail.com>
2019-05-24 19:40:25 -0400

    Insert basic webserver main.go

---
+++ cmd/plex-handler/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"net/http"
+	_ "net/http/pprof"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/go-kit/kit/log"
+)
+
+func main() {
+
+	// Config.
+	var (
+		httpAddr  = flag.String("http.addr", ":3000", "HTTP listen address")
+		debugAddr = flag.String("debug.addr", ":3001", "Debug and metrics listen address")
+	)
+	flag.Parse()
+
+	// Logging.
+	var logger log.Logger
+	{
+		logger = log.NewLogfmtLogger(os.Stderr)
+		logger = log.With(logger, "ts", log.DefaultTimestampUTC)
+		logger = log.With(logger, "caller", log.DefaultCaller)
+	}
+
+	// Mechanical.
+	errc := make(chan error)
+	_, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// Interrupt.
+	go func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+		errc <- fmt.Errorf("%s", <-c)
+	}()
+
+	// Debug.
+	go func() {
+		logger := log.With(logger, "transport", "debug")
+		logger.Log("addr", *debugAddr)
+		errc <- http.ListenAndServe(*debugAddr, nil)
+	}()
+
+	// App.
+	go func() {
+		var srv http.Server
+
+		logger := log.With(logger, "transport", "http")
+		logger.Log("addr", *httpAddr)
+
+		// Server config
+		{
+			srv.Addr = *httpAddr
+			srv.Handler = HandlerGetter()
+			srv.ReadTimeout = time.Second * 30
+			srv.WriteTimeout = time.Second * 30
+		}
+
+		errc <- srv.ListenAndServe()
+	}()
+
+	// Run.
+	logger.Log("exit", <-errc)
+}
