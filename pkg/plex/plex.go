aeb939e76fcac89050f9498530808d42bdbf4240
Cory Locklear <corylocklear@gmail.com>
2019-05-25 14:44:46 -0400

    Initial implementation of ActivityStore

--- pkg/plex/plex.go
+++ pkg/plex/plex.go
@@ -1,5 +1,10 @@
 package plex
 
+import (
+	"encoding/json"
+	"io"
+)
+
 // WebhookPayload represents a payload from a plex webhook
 type WebhookPayload struct {
 	Event   string `json:"event"`
@@ -48,27 +53,45 @@ type WebhookPayload struct {
 	} `json:"Metadata"`
 }
 
-// FileActivityStore is used to read/write webhook activity
-type FileActivityStore struct {
-	MaxItems int
-	Path     string
+// ActivityStore is used to read/write webhook activity to/from the given ReadWriter
+type ActivityStore struct {
+	maxItems int
+	store    io.ReadWriter
+	items    []WebhookPayload
 }
 
-// NewFileActivityStore creates a new FileActivityStore instance backed by the given file
-func NewFileActivityStore(filePath string, maxItems int) *FileActivityStore {
-	s := FileActivityStore{
-		MaxItems: maxItems,
-		Path:     filePath,
+// NewActivityStore creates an instance backed by the given ReadWriter
+func NewActivityStore(store io.ReadWriter, maxItems int) (*ActivityStore, error) {
+	s := ActivityStore{
+		maxItems: maxItems,
+		store:    store,
 	}
-	return &s
+	err := s.reload()
+	if err != nil {
+		return nil, err
+	}
+	return &s, nil
 }
 
-// Read is FileActivityStore's implementation of io.Reader
-func (s *FileActivityStore) Read(p []byte) (n int, err error) {
-
+func (s *ActivityStore) reload() error {
+	// Load the contents from the store
+	return json.NewDecoder(s.store).Decode(&s.items)
 }
 
-// Write is FileActivityStore's implementation of io.Writer
-func (s *FileActivityStore) Write(p []byte) (n int, err error) {
+func (s *ActivityStore) commit() error {
+	return json.NewEncoder(s.store).Encode(s.items)
+}
 
+// GetAll returns all items in the ActivityStore
+func (s *ActivityStore) GetAll() []WebhookPayload {
+	return s.items
+}
+
+// Add appends give given WebhookPayload to the ActivityStore.  If this causes the size of the store to exceed maxItems, the oldest item is removed.
+func (s *ActivityStore) Add(wh WebhookPayload) error {
+	s.items = append(s.items, wh)
+	if len(s.items) > s.maxItems {
+		s.items = s.items[(len(s.items) - s.maxItems):]
+	}
+	return s.commit()
 }
