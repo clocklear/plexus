e8c8236f61aae4ff5ea2b0115906933208b2168b
Cory Locklear <corylocklear@gmail.com>
2019-05-29 22:53:05 -0400

    Properly handle multipart form from Plex

--- cmd/plexus/http/handler.go
+++ cmd/plexus/http/handler.go
@@ -3,7 +3,9 @@ package http
 import (
 	"encoding/json"
 	"io/ioutil"
+	"mime"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/go-kit/kit/log"
@@ -39,17 +41,54 @@ func handleHealthCheck() func(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func hasContentType(r *http.Request, mimetype string) bool {
+	contentType := r.Header.Get("Content-type")
+	if contentType == "" {
+		return mimetype == "application/octet-stream"
+	}
+
+	for _, v := range strings.Split(contentType, ",") {
+		t, _, err := mime.ParseMediaType(v)
+		if err != nil {
+			break
+		}
+		if t == mimetype {
+			return true
+		}
+	}
+	return false
+}
+
 func handlePlexWebhook(v *schema.Validator, store *plex.Store, cfg plex.Config) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		logger := r.Context().Value(keyLogger).(log.Logger)
 		reqID := r.Context().Value(keyRequestID).(string)
 
-		// Extract request body
-		payload, err := ioutil.ReadAll(r.Body)
-		defer r.Body.Close()
+		// https://support.plex.tv/articles/115002267687-webhooks/
+		// Per their documentation, Plex will send a multipart form request, and 'payload' is the JSON of the hook
+		// We want to be flexible (makes testing easier), so lets see if we can handle both scenarios (multipart vs raw JSON post)
+		payload := []byte{}
+		if hasContentType(r, "multipart/form-data") {
+			err := r.ParseMultipartForm(10 * 1024 * 1024) // 10mb
+			if err != nil {
+				Failure(w, err, http.StatusBadRequest, logger)
+				return
+			}
+			payload = []byte(r.FormValue("payload"))
+		} else {
+			// Assume raw JSON post
+			pl, err := ioutil.ReadAll(r.Body)
+			defer r.Body.Close()
+			if err != nil {
+				Failure(w, err, http.StatusInternalServerError, logger)
+				return
+			}
+			payload = pl
+		}
 
+		// Should have JSON bytes by this point
 		// Validate the request
-		err = v.Validate(payload)
+		err := v.Validate(payload)
 		if err != nil {
 			Failure(w, err, http.StatusBadRequest, logger)
 			return
